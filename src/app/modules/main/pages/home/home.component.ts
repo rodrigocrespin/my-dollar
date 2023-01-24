24d9ac5d221f42e267501cf48d74796d2d2c56cc
Rodrigo Crespin <rodcrespin@gmail.com>
2023-01-24 23:14:16 +0100

    add usd price change

--- src/app/modules/main/pages/home/home.component.ts
+++ src/app/modules/main/pages/home/home.component.ts
@@ -3,6 +3,8 @@ import { combineLatest, Observable } from 'rxjs';
 import { Settings } from 'src/app/models/settings';
 import { SettingsService } from 'src/app/services/settings.service';
 import { map, startWith } from 'rxjs/operators';
+import { PriceChangesService } from '../../../../services/price-changes.service';
+import { PriceChange } from '../../../../models/price-change';
 
 interface HomeModel {
   settings: Settings;
@@ -16,10 +18,15 @@ interface HomeModel {
 
 export class HomeComponent {
   model$: Observable<HomeModel>;
+  usdPriceChange$: Observable<{ loading: boolean, priceChange?: PriceChange }>;
   showHistory = false;
 
-  constructor(private settingsService: SettingsService) {
+  constructor(private settingsService: SettingsService, private priceChangesService: PriceChangesService) {
     const settings$ = this.settingsService.get();
+    this.usdPriceChange$ = this.priceChangesService.get('USD').pipe(
+      map(priceChange => ({ priceChange, loading: false })),
+      startWith({ loading: true })
+    );
 
     this.model$ = combineLatest([settings$]).pipe(
       map(([settings]) => ({ settings, loading: false })),
