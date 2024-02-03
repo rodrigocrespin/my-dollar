76e221ed024f7e7cf173a0f58f89aa32d5e28815
Rodrigo Crespin <rodcrespin@gmail.com>
2024-02-03 20:36:38 +0100

    add chart

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -4,16 +4,20 @@ import { HomeComponent } from './components/home/home.component';
 import { MainRoutingModule } from './main-routing.module';
 import { ExchangeRateCardComponent } from './components/exchange-rate-card/exchange-rate-card.component';
 import { SharedModule } from '../../shared/shared.module';
+import { HistoricalExchangeRatesChartComponent } from './components/historical-exchange-rates-chart/historical-exchange-rates-chart.component';
+import { NgApexchartsModule } from 'ng-apexcharts';
 
 @NgModule({
   declarations: [
     HomeComponent,
     ExchangeRateCardComponent,
+    HistoricalExchangeRatesChartComponent,
   ],
   imports: [
     CommonModule,
     MainRoutingModule,
     SharedModule,
+    NgApexchartsModule,
   ]
 })
 export class MainModule { }
